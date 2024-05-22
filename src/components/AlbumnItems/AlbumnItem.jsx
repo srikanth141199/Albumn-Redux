f111daa74c5a6e150ca9fb74b0c99ea84511d913
Srikanth K <kolleparasrikanth@gmail.com>
2024-05-22 21:04:07 +0530

    Update Implemented

--- src/components/AlbumnItems/AlbumnItem.jsx
+++ src/components/AlbumnItems/AlbumnItem.jsx
@@ -1,22 +1,36 @@
 import { useDispatch } from "react-redux"
+import { useState } from "react";
 import "./AlbumnItem.css"
-import { albumnSelector, deleteItem, updateItem } from "../redux/Reducers/AlbumnReducer"
+import { albumnSelector, deleteItem } from "../redux/Reducers/AlbumnReducer"
+import ItemUpdate from "../ItemUpdate/ItemUpdate";
+import Modal from "../Modal/Modal"; // Import the Modal component
 
-export default function AlbumnItem({id, userId, title}){
+export default function AlbumnItem({ id, userId, title }) {
     const dispatch = useDispatch(albumnSelector);
+    const [isUpdating, setIsUpdating] = useState(false);
 
-    const handleDelete = ()=>{
-        dispatch(deleteItem(id))
+    const handleDelete = () => {
+        dispatch(deleteItem(id));
     }
 
-    const handleUpdate = () => {
-        dispatch(updateItem({id, userId, title}))
+    const handleUpdateClick = () => {
+        setIsUpdating(true); // Show the modal
     }
-    return(
-        <div className="AlbumnItem">
+
+    const closeModal = () => {
+        setIsUpdating(false); // Hide the modal
+    }
+
+    return (
+        <div className="AlbumnItem albumn-grid">
             <h4 className="title">{title}</h4>
-            <button className="update" onClick={() => handleUpdate(id)}>update</button>
+            <button className="update" onClick={handleUpdateClick}>Update</button>
             <button className="delete" onClick={() => handleDelete()}>Delete</button>
+            {isUpdating && (
+                <Modal onClose={closeModal}>
+                    <ItemUpdate id={id} title = {title} closeModal={closeModal} />
+                </Modal>
+            )}
         </div>
-    )
+    );
 }
---
+++ src/components/ItemUpdate/ItemUpdate.jsx
@@ -0,0 +1,29 @@
+import { useDispatch } from "react-redux"
+import { updateItem } from "../redux/Reducers/AlbumnReducer";
+import { useRef } from "react";
+
+export default function ItemUpdate({ id, title, closeModal }) {
+    console.log("Inside Item Update");
+
+    const userIdRef = useRef();
+    const titleRef = useRef();
+
+    const dispatch = useDispatch();
+    const handleSubmit = () => {
+        const userId = userIdRef.current.value;
+        const title = titleRef.current.value;
+        dispatch(updateItem({ id, userId, title }));
+        closeModal(); // Close the modal after submitting
+    }
+
+    return (
+        <div className="ItemContainer">
+            <h4 className="title">{title}</h4>
+            <label>Enter new User ID: </label>
+            <input type="number" ref={userIdRef} />
+            <label>Enter new Title: </label>
+            <input type="text" ref={titleRef} />
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    );
+}
